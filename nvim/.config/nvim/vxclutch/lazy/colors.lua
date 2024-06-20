e116ae5e0bde777331115ae2546bcfc041a421bf
vx_clutch <98831688+vx-clutch@users.noreply.github.com>
2024-06-20 10:40:51 -0400

    oohh colors

--- nvim/.config/nvim/vxclutch/lazy/colors.lua
+++ nvim/.config/nvim/vxclutch/lazy/colors.lua
@@ -1,14 +1,3 @@
 return {
-    {
-        "rose-pine/neovim",
-        config = function()
-            function ColorMyPencils(color)
-                color = color or "rose-pine"
-                vim.cmd.colorscheme(color)
-                vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-                vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-            end
-            ColorMyPencils()
-        end
-    }
+    "catppuccin/nvim", name = "catppuccin", priority = 1000
 }

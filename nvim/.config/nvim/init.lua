643b047d2ce532cdda77347081c9fc972e36e30f
vx-clutch <owestness@gmail.com>
2024-06-22 20:30:19 -0400

    feat: Wex

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -910,3 +910,9 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+--
+-- Custom Commands
+vim.api.nvim_create_user_command('Wex', function()
+  vim.cmd 'w'
+  vim.cmd 'Oil'
+end, {})

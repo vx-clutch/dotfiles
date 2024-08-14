f38dd555e5da6ebfed355ad63fecef074afc5978
vx-clutch <owestness@gmail.com>
2024-08-13 22:03:34 -0400

    save

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -401,6 +401,9 @@ require('lazy').setup({
       -- used for completion, annotations and signatures of Neovim apis
       { 'folke/neodev.nvim', opts = {} },
     },
+    {
+      'hrsh7th/cmp-nvim-lsp',
+    },
     config = function()
       -- Brief aside: **What is LSP?**
       --
@@ -748,24 +751,17 @@ require('lazy').setup({
         },
       }
     end,
-  },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'catppuccin/nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'catppuccin'
+    {
+      'maxmx03/solarized.nvim',
+      lazy = false,
+      priority = 1000,
+      config = function()
+        vim.o.background = 'dark' -- or 'light'
 
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
-    end,
+        vim.cmd.colorscheme 'solarized'
+      end,
+    },
   },
 
   -- Highlight todo, notes, etc in comments

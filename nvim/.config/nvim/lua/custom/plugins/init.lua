dc8caa42a54eef17355b99ff9db67e11dd353b7c
vx-clutch <owestness@gmail.com>
2024-08-23 19:04:37 -0400

    save

--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -21,7 +21,7 @@ return {
       -- REQUIRED
 
       vim.keymap.set('n', '<leader>a', function()
-        harpoon:list():append()
+        harpoon:list():add()
       end, { desc = 'Append to harpoon' })
       vim.keymap.set('n', '<C-e>', function()
         harpoon.ui:toggle_quick_menu(harpoon:list())

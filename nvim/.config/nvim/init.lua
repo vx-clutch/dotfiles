3f938b9b1e7a4dd6a4426a8767be6d16bd3b7438
vx-clutch <owestness@gmail.com>
2024-09-24 17:36:47 -0400

    save

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -167,6 +167,9 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- File kemaps
+vim.keymap.set('n', '<leader>-', '<cmd>sp<CR>-', { desc = 'Open file navigation' })
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.

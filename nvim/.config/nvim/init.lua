a6c38143adcfb5c5edc6a79b56bbf467e58c766c
vx-clutch <owestness@gmail.com>
2024-09-30 15:10:30 -0400

    save

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -485,11 +485,6 @@ require("lazy").setup({
 				desc = "Buffer Diagnostics (Trouble)",
 			},
 			{
-				"<leader>cs",
-				"<cmd>Trouble symbols toggle focus=false<cr>",
-				desc = "Symbols (Trouble)",
-			},
-			{
 				"<leader>cl",
 				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
 				desc = "LSP Definitions / references / ... (Trouble)",

7772c2f7c07b2d981bd2788f89ae483d54efc821
vx-clutch <owestness@gmail.com>
2024-12-19 21:56:32 -0500

    save

--- nvim/.config/nvim/lua/vxclutch/lazy/dap.lua
+++ nvim/.config/nvim/lua/vxclutch/lazy/dap.lua
@@ -1,61 +1,83 @@
 return {
-	{
-		"mfussenegger/nvim-dap",
-		dependencies = {
-			"leoluz/nvim-dap-go",
-			"rcarriga/nvim-dap-ui",
-			"nvim-neotest/nvim-nio",
-			"williamboman/mason.nvim",
-			"julianolf/nvim-dap-lldb",
-			"mfussenegger/nvim-python-dap",
-		},
-		config = function()
-			local dap = require("dap")
-			local ui = require("dapui")
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "leoluz/nvim-dap-go",
+      "rcarriga/nvim-dap-ui",
+      "nvim-neotest/nvim-nio",
+      "williamboman/mason.nvim",
+      "julianolf/nvim-dap-lldb",
+      "mfussenegger/nvim-dap-python",
+    },
+    config = function()
+      local dap = require "dap"
+      local ui = require "dapui"
 
-			require("dapui").setup()
-			require("dap-go").setup()
-			require("dap-python").setup()
-			require("dap-lldb").setup()
+      require("dapui").setup()
+      require("dap-go").setup()
+      require("dap-python").setup()
+      require("dap-lldb").setup()
 
+      -- Handled by nvim-dap-go
+      -- dap.adapters.go = {
+      --   type = "server",
+      --   port = "${port}",
+      --   executable = {
+      --     command = "dlv",
+      --     args = { "dap", "-l", "127.0.0.1:${port}" },
+      --   },
+      -- }
 
-			-- Handled by nvim-dap-go
-			-- dap.adapters.go = {
-			--   type = "server",
-			--   port = "${port}",
-			--   executable = {
-			--     command = "dlv",
-			--     args = { "dap", "-l", "127.0.0.1:${port}" },
-			--   },
-			-- }
+      dap.adapters.lldb = {
+        type = "executable",
+        command = "/usr/bin/lldb", -- Adjust path if necessary
+        name = "lldb",
+      }
 
-			vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
-			vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
+      dap.configurations.cpp = {
+        {
+          name = "Launch",
+          type = "lldb",
+          request = "launch",
+          program = function()
+            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+          end,
+          cwd = "${workspaceFolder}",
+          stopOnEntry = false,
+          args = {},
+          runInTerminal = false,
+        },
+      }
+      dap.configurations.c = dap.configurations.cpp
+      dap.configurations.rust = dap.configurations.cpp
 
-			-- Eval var under cursor
-			vim.keymap.set("n", "<space>?", function()
-				require("dapui").eval(nil, { enter = true })
-			end)
+      vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
+      vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
 
-			vim.keymap.set("n", "<leader>dc", dap.continue)
-			vim.keymap.set("n", "<leader>s", dap.step_into)
-			vim.keymap.set("n", "<leader>n", dap.step_over)
-			vim.keymap.set("n", "<leader>do", dap.step_out)
-			vim.keymap.set("n", "<leader>db", dap.step_back)
-			vim.keymap.set("n", "<leader>dr", dap.restart)
+      -- Eval var under cursor
+      vim.keymap.set("n", "<space>?", function()
+        require("dapui").eval(nil, { enter = true })
+      end)
 
-			dap.listeners.before.attach.dapui_config = function()
-				ui.open()
-			end
-			dap.listeners.before.launch.dapui_config = function()
-				ui.open()
-			end
-			dap.listeners.before.event_terminated.dapui_config = function()
-				ui.close()
-			end
-			dap.listeners.before.event_exited.dapui_config = function()
-				ui.close()
-			end
-		end,
-	},
+      vim.keymap.set("n", "<leader>dc", dap.continue)
+      vim.keymap.set("n", "<leader>s", dap.step_into)
+      vim.keymap.set("n", "<leader>n", dap.step_over)
+      vim.keymap.set("n", "<leader>do", dap.step_out)
+      vim.keymap.set("n", "<leader>db", dap.step_back)
+      vim.keymap.set("n", "<leader>dr", dap.restart)
+
+      dap.listeners.before.attach.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        ui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        ui.close()
+      end
+    end,
+  },
 }

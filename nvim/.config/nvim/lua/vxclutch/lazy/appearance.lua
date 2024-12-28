07d6ce92f29a44199c6cb6ea9166ff33862ee215
vx-clutch <owestness@gmail.com>
2024-12-27 21:05:47 -0500

    stole from prime

--- nvim/.config/nvim/lua/vxclutch/lazy/appearance.lua
+++ nvim/.config/nvim/lua/vxclutch/lazy/appearance.lua
@@ -1,74 +1,90 @@
+-- taken directly from ThePrimeagen/init.lua
+function ColorMyPencils(color)
+  color = color or "rose-pine-moon"
+  vim.cmd.colorscheme(color)
+
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
 return {
+
   {
-    "nvim-lualine/lualine.nvim",
-    requires = { "kyazdani42/nvim-web-devicons" },
-    dependencies = {
-      "stevearc/dressing.nvim",
-      "norcalli/nvim-colorizer.lua",
-      "rktjmp/lush.nvim",
-      "zaldih/themery.nvim",
-    },
+    "erikbackman/brightburn.vim",
+  },
+
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    opts = {},
     config = function()
-      require("lualine").setup {
-        options = {
-          theme = "auto",
-          component_separators = "",
-          section_separators = "",
+      ColorMyPencils()
+    end,
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
+    name = "gruvbox",
+    config = function()
+      require("gruvbox").setup {
+        terminal_colors = true, -- add neovim terminal colors
+        undercurl = true,
+        underline = false,
+        bold = true,
+        italic = {
+          strings = false,
+          emphasis = false,
+          comments = false,
+          operators = false,
+          folds = false,
         },
-        sections = {
-          lualine_a = { "mode" },
-          lualine_b = {},
-          lualine_c = { "filename" },
-          lualine_x = {},
-          lualine_y = {},
-          lualine_z = {
-            function()
-              return "[" .. vim.bo.filetype .. "]"
-            end,
-          },
-        },
-      }
-      require("colorizer").setup {
-        "*",
-        css = { rgb_fn = true },
-      }
-      require("dressing").setup {
-        select = {
-          backend = { "nui", "telescope", "builtin" },
-        },
-      }
-      require("themery").setup {
-        themes = {
-          "aquarium",
-          "vague",
-          "catppuccin-mocha",
-          "gruvbox",
-          "catppuccin-frappe",
-          "catppuccin-macchiato",
-          "nord",
-          "rose-pine",
-          "tokyonight",
-          "bluloco-dark",
-          "vesper",
-          "poimandres",
-          "oxocarbon",
-        },
-        livePreview = true,
+        strikethrough = true,
+        invert_selection = false,
+        invert_signs = false,
+        invert_tabline = false,
+        invert_intend_guides = false,
+        inverse = true, -- invert background for search, diffs, statuslines and errors
+        contrast = "", -- can be "hard", "soft" or empty string
+        palette_overrides = {},
+        overrides = {},
+        dim_inactive = false,
+        transparent_mode = false,
       }
     end,
   },
   {
-    -- themes
-    "FrenzyExists/aquarium-vim",
-    "vague2k/vague.nvim",
-    "catppuccin/nvim",
-    "morhetz/gruvbox",
-    "shaunsingh/nord.nvim",
-    "rose-pine/neovim",
     "folke/tokyonight.nvim",
-    "uloco/bluloco.nvim",
-    "gambhirsharma/vesper.nvim",
-    "olivercederborg/poimandres.nvim",
-    "nyoom-engineering/oxocarbon.nvim",
+    config = function()
+      require("tokyonight").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        style = "storm", -- The theme comes in three styles, storm, moon, a darker variant night and day
+        transparent = true, -- Enable this to disable setting the background color
+        terminal_colors = true, -- Configure the colors used when opening a :terminal in Neovim
+        styles = {
+          -- Style to be applied to different syntax groups
+          -- Value is any valid attr-list value for :help nvim_set_hl
+          comments = { italic = false },
+          keywords = { italic = false },
+          -- Background styles. Can be "dark", "transparent" or "normal"
+          sidebars = "dark", -- style for sidebars, see below
+          floats = "dark", -- style for floating windows
+        },
+      }
+    end,
+  },
+
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      require("rose-pine").setup {
+        disable_background = true,
+        styles = {
+          italic = false,
+        },
+      }
+
+      ColorMyPencils()
+    end,
   },
 }

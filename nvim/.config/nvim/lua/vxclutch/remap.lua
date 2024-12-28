dc729e6585d79e3f7e6f9161ad7a79df4ed5113e
vx-clutch <owestness@gmail.com>
2024-12-28 14:40:51 -0500

    save

--- nvim/.config/nvim/lua/vxclutch/remap.lua
+++ nvim/.config/nvim/lua/vxclutch/remap.lua
@@ -31,11 +31,3 @@ set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
-
--- Theme
-set(
-	"n",
-	"<leader><C-t>",
-	":Themery<CR>",
-	{ noremap = true, silent = false, desc = "Change the current theme" }
-)

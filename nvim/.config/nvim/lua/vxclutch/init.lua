0918edd0657e4cf3e4070e2e89a036ccac31fa80
vx-clutch <owestness@gmail.com>
2025-01-04 09:24:35 -0500

    save

--- nvim/.config/nvim/lua/vxclutch/init.lua
+++ nvim/.config/nvim/lua/vxclutch/init.lua
@@ -1,5 +1,14 @@
 vim.g.mapleader = " "
 
-require("vxclutch.set")
-require("vxclutch.lazy_init")
-require("vxclutch.remap")
+-- https://github.com/vx-clutch/ftl
+vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
+    pattern = "*.ftl",
+    callback = function()
+        vim.bo.filetype = "c"
+    end,
+})
+
+
+require "vxclutch.set"
+require "vxclutch.lazy_init"
+require "vxclutch.remap"
--- nvim/.config/nvim/lua/vxclutch/lazy/telescope.lua
+++ nvim/.config/nvim/lua/vxclutch/lazy/telescope.lua
@@ -11,10 +11,10 @@ return {
     require("telescope").setup {}
 
     local builtin = require "telescope.builtin"
-    vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
+    vim.keymap.set("n", "<leader><leader>", builtin.find_files, {})
     vim.keymap.set("n", "<C-p>", builtin.git_files, {})
     vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
-    vim.keymap.set("n", "<leader><leader>", function()
+    vim.keymap.set("n", "<leader>sg", function()
       builtin.live_grep {
         grep_open_files = true,
         prompt_title = "Live Grep in Open Files",

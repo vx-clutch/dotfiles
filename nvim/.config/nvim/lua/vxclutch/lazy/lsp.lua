1fcf856ab3f54c661f2037e764d7dd9427735466
vx-clutch <owestness@gmail.com>
2025-05-02 18:33:34 -0400

    save

--- nvim/.config/nvim/lua/vxclutch/lazy/lsp.lua
+++ nvim/.config/nvim/lua/vxclutch/lazy/lsp.lua
@@ -1,50 +1,91 @@
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    "stevearc/conform.nvim",
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "j-hui/fidget.nvim",
-  },
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"j-hui/fidget.nvim",
+			"stevearc/conform.nvim",
 
-  config = function()
-    require("conform").setup({
-      formatters_by_ft = {},
-    })
+			-- Completion-related plugins
+			"hrsh7th/nvim-cmp",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+		},
 
-    require("fidget").setup({})
-    require("mason").setup()
+		config = function()
+			-- Autocomplete Setup
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    require("mason-lspconfig").setup({
-      ensure_installed = {
-        "lua_ls",
-        "clangd",
-        "gopls",
-      },
-      handlers = {
-        function(server_name)
-          require("lspconfig")[server_name].setup({})
-        end,
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<Tab>"] = cmp.mapping.select_next_item(),
+					["<S-Tab>"] = cmp.mapping.select_prev_item(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<C-Space>"] = cmp.mapping.complete(),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+				}, {
+					{ name = "buffer" },
+					{ name = "path" },
+				}),
+			})
 
-        ["lua_ls"] = function()
-          require("lspconfig").lua_ls.setup({
-            settings = {
-              Lua = {
-                runtime = { version = "Lua 5.1" },
-                diagnostics = {
-                  globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
-                },
-              },
-            },
-          })
-        end,
+			-- LSP + Tools Setup
+			require("conform").setup({
+				formatters_by_ft = {},
+			})
 
-        ["clangd"] = function()
-          require("lspconfig").clangd.setup({
-            cmd = { "clangd", "--header-insertion=iwyu" },
-          })
-        end,
-      },
-    })
-  end,
+			require("fidget").setup({})
+			require("mason").setup()
+
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"clangd",
+					"gopls",
+				},
+				handlers = {
+					function(server_name)
+						require("lspconfig")[server_name].setup({
+							capabilities = capabilities,
+						})
+					end,
+
+					["lua_ls"] = function()
+						require("lspconfig").lua_ls.setup({
+							capabilities = capabilities,
+							settings = {
+								Lua = {
+									runtime = { version = "Lua 5.1" },
+									diagnostics = {
+										globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
+									},
+								},
+							},
+						})
+					end,
+
+					["clangd"] = function()
+						require("lspconfig").clangd.setup({
+							capabilities = capabilities,
+							cmd = { "clangd", "--header-insertion=iwyu" },
+						})
+					end,
+				},
+			})
+		end,
+	},
 }

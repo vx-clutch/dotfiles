834fd3ff608374b2fd0f9f4704709b0d161fb877
vx-clutch <owestness@gmail.com>
2025-07-23 12:35:58 -0400

    save

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -21,40 +21,6 @@ config.window_padding = {
 	bottom = 0,
 }
 
-config.color_scheme = "vx-rose-pine"
-
-config.color_schemes = {
-	["vx-rose-pine"] = {
-		foreground = "#e0def4",
-		background = "#000000",
-		cursor_bg = "#e0def4",
-		cursor_border = "#e0def4",
-		cursor_fg = "#191724",
-		selection_bg = "#524f67",
-		selection_fg = "#e0def4",
-		ansi = {
-			"#191724", -- Base
-			"#eb6f92", -- Love
-			"#f6c177", -- Gold
-			"#ebbcba", -- Rose
-			"#9ccfd8", -- Pine
-			"#c4a7e7", -- Iris
-			"#31748f", -- Foam
-			"#e0def4", -- Text
-		},
-		brights = {
-			"#6e6a86", -- Subtle
-			"#eb6f92", -- Love
-			"#f6c177", -- Gold
-			"#ebbcba", -- Rose
-			"#9ccfd8", -- Pine
-			"#c4a7e7", -- Iris
-			"#31748f", -- Foam
-			"#e0def4", -- Text
-		},
-	},
-}
-
 config.font = wezterm.font("Iosevka Slab")
 config.font_size = 20.0
 

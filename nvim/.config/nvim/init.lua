04a59afff7e9d3853a8a1798c056958f4f394365
vx-clutch <owestness@gmail.com>
2025-07-28 16:57:30 -0400

    save

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -164,15 +164,6 @@ for _, server in ipairs(servers) do
   lspconfig[server].setup({})
 end
 
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(ev)
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    if client:supports_method('textDocument/completion') then
-      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-    end
-  end,
-})
-
 vim.diagnostic.config({
   virtual_text = true,
   underline = true,

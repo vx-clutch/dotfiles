55fb5e3756e07960947b5916f8e802ade32bb940
vx-clutch <owestness@gmail.com>
2025-10-02 20:16:12 -0400

    big changes

--- Brewfile
+++
@@ -1,4 +0,0 @@
-tap "homebrew/cask-fonts"
-
-brew "neovim", args: ["HEAD"]
-cask "font-comic-shanns-mono-nerd-font"
--- plug.sh
+++
@@ -1,1 +0,0 @@
-zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1

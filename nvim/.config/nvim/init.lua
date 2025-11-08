81a6c323d656769a6f89824dfb0db4cc20c622b0
vx-clutch <owestness@gmail.com>
2025-11-08 13:06:50 -0500

    save

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -147,7 +147,7 @@ require("lazy").setup({
 	local builtin = require("telescope.builtin")
       	require("telescope").setup {}
 	vim.keymap.set("n", "<leader>g", builtin.live_grep)
-	vim.keymap.set("n", "<leader>s", builtin.find_files)
+	vim.keymap.set("n", "<leader>f", builtin.find_files)
       end
     },
 })
